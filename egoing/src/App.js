d09e481892b904144eb582b839cc67a917778d38
bumjin <potato980124@gmail.com>
2023-07-06 22:50:00 +0900

    07/06

--- egoing/src/App.js
+++ egoing/src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
+  import './App.css';
 import { useState } from 'react'; 
 function App() {
   let [mode,setMode] = useState("wellcome");
   let content = null;
+  let contextControll = null;
   let [id,setId] = useState(null);
-  const topics = [
+  let [nextId,setNextId] = useState(4);
+  let [topics,setTopics] = useState([
     {id:1,title:'html',content:"html is ..."},
     {id:2,title:'css',content:"css is ..."},
     {id:3,title:'js',content:"js is ..."},
-  ]
+  ])
   if(mode === "wellcome"){
     content = <Article title ="welcome" body = "hello,web"></Article> 
   }else if(mode ==="READ"){
@@ -20,7 +22,18 @@ function App() {
         body = topics[i].content;
       }
     }
-    content = <Article title ={title} body = {body}></Article>  
+    content = <Article title ={title} body = {body}></Article>; 
+    contextControll = <li><a href='/update'>update</a></li>; 
+  }else if(mode === 'CREATE'){
+    content = <Create onCreate={(title,body)=>{
+    let newTopic = {id:nextId,title:title,content:body};
+    let newTopics = [...topics];
+    newTopics.push(newTopic);
+    setTopics(newTopics);
+    setMode("READ");
+    setId(nextId);
+    setNextId(nextId + 1);  
+    }}></Create>
   }
   return (
     <div>
@@ -29,11 +42,35 @@ function App() {
         setMode('READ');
         setId(id);
     }}></Nav>
-      {content} 
+      <p>---------------</p>
+      {content}
+      <p>---------------</p>
+      <li> 
+      <a href='/create' onClick={event => {
+        event.preventDefault();
+        setMode('CREATE');
+      }}>create</a>
+      </li>
+      {contextControll}
     </div>
   );
 }
-
+function Create(props){
+  return <article>
+    <h2>등록할 제목과 내용을 입력하세요~</h2>
+    <form onSubmit={event =>{
+      event.preventDefault();
+      console.log(event.target.tilte);
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title,body);
+    }}>
+      <p><input type='text' name='title' placeholder='title'/></p>
+      <p><textarea name='body' placeholder='contetnt'></textarea></p>
+      <p><input type='submit' value="Create"/></p>
+    </form>
+  </article>
+}
 function Header(props){
   return <header>
         <h1><a href="/" onClick={(event)=>{ 
